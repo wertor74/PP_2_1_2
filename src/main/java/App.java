c07a2ef814d8251c7ba6e4d8f1561ef33afb6dba
wertor <wertor74@gmail.com>
2022-06-11 00:41:34 +0300

    Start PP_2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1);
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2);
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3);
+        boolean compareBean = bean == bean1;
+        System.out.println(compareBean);
+        compareBean = bean2 == bean3;
+        System.out.println(compareBean);
     }
 }
